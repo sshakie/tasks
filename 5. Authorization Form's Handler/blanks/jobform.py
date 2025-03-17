a1e949c56c10ded5d06b292e6a1148db01aa969b
shaki <shakierdk@mail.ru>
2025-03-18 02:32:16 +0300

    did add_job form

---
+++ 5. Authorization Form's Handler/blanks/jobform.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import *
+from wtforms.validators import *
+
+
+class JobForm(FlaskForm):
+    team_leader = IntegerField('id лидера', validators=[DataRequired()])
+    job = StringField('Работа', validators=[DataRequired()])
+    work_size = IntegerField('Длительность', validators=[DataRequired()])
+    collaborators = StringField('Сотрудники', validators=[DataRequired()])
+    end_date = DateTimeField('Дедлайн')
+    is_finished = BooleanField('Закончена')
+    submit = SubmitField('Добавить')
