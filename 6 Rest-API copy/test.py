4866bbc05686ad944676b2a4be92591f3a5c84f3
shaki <shakierdk@mail.ru>
2025-04-14 17:49:51 +0300

    fixed test in rest api copy

--- 6 Rest-API copy/test.py
+++ 6 Rest-API copy/test.py
@@ -1,35 +1,34 @@
 from requests import get, post, delete
 import json
 
-
 print(post('http://localhost:5000/api/jobs',
-           json={'job': 'da', 'work_size': 1, 'collaborators': '1,2', 'is_finished': True}).json(), '+')  # все работает
+           json={'job': 'da', 'team_leader': 1, 'work_size': 1, 'collaborators': '1,2', 'is_finished': True}).json(),
+      '+')  # все работает
 
-
-print(get('http://localhost:5000/api/jobs').json(), '+') # правильно
-print(get('http://localhost:5000/api/jobs/1').json(), '+') # правильно
+print(get('http://localhost:5000/api/jobs').json(), '+')  # правильно
+print(get('http://localhost:5000/api/jobs/1').json(), '+')  # правильно
 print(get('http://localhost:5000/api/jobs/10').json(), '-')  # нет такого
 print(get('http://localhost:5000/api/jobs/a').json(), '-')  # неверный тип
 
-
 print(post('http://localhost:5000/api/jobs',
-           json={'job': 'da', 'work_size': 1}).json(), '-')  # не хватает обязательного компонента
+           json={'job': 'da', 'work_size': 1}).json(), '-')  # не хватает 2 обязательных компонента
 print(post('http://localhost:5000/api/jobs',
            json={}).json(), '-')  # пустой json
 print(post('http://localhost:5000/api/jobs',
-           json={'job': 'da', 'work_size': 1, 'collaborators': '1,2', 'is_finished': True,
+           json={'job': 'da', 'team_leader': 1, 'work_size': 1, 'collaborators': '1,2', 'is_finished': True,
                  'abc': 'cba'}).json(), '-')  # лишний запрос
 
-
 print(post('http://localhost:5000/api/jobs/edit/1',
-           json={'job': 'da', 'work_size': 1, 'collaborators': '1,2', 'is_finished': True}).json(), '+')  # все работает
+           json={'job': 'da', 'team_leader': 1, 'work_size': 1, 'collaborators': '1,2', 'is_finished': True}).json(),
+      '+')  # все работает
 print(post('http://localhost:5000/api/jobs/edit/100',
-           json={'job': 'da', 'work_size': 1, 'collaborators': '1,2', 'is_finished': True}).json(), '-')  # нет такого id
+           json={'job': 'da', 'team_leader': 1, 'work_size': 1, 'collaborators': '1,2', 'is_finished': True}).json(),
+      '-')  # нет такого id
 print(post('http://localhost:5000/api/jobs/edit/str',
-           json={'job': 'da', 'work_size': 1, 'collaborators': '1,2', 'is_finished': True}).json(), '-')  # неверный тип
+           json={'job': 'da', 'team_leader': 1, 'work_size': 1, 'collaborators': '1,2', 'is_finished': True}).json(),
+      '-')  # неверный тип
 print(post('http://localhost:5000/api/jobs/edit/1',
            json={}).json(), '-')  # пустой json
 print(post('http://localhost:5000/api/jobs/edit/1',
            json={'job': 'da', 'abc': 'cba'}).json(), '-')  # лишний запрос
 print(get('http://localhost:5000/api/jobs').json(), '+')  # вывод
-
